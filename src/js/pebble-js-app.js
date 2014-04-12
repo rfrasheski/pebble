c0d47968701a8a22dd77a17f8321fc9118c865f9
Tworley <tdworley360@yahoo.com>
2014-04-12 14:48:17 -0700

    Js update

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,9 +1,9 @@
-function searchRecipe(term){
+function searchRecipe(numid){
   var response;
   var req = new XMLHttpRequest();
   var apiKey = "your-api-key-here";
-  var titleKeyword = "lasagna";
-  var url = "http://api.bigoven.com/recipes?pg=1&rpp=25&title_kw=" + titleKeyword + "&api_key="+apiKey;
+  var id = numid;
+  var url = "http://api.bigoven.com/recipe/" + id + "&api_key="+apiKey;
   req.open("GET",url,true);
   req.onload = function (e){
     if(req.readyState == 4){
@@ -12,12 +12,13 @@ function searchRecipe(term){
         response = JSON.parse(req.responseText);
       }
     }
-  }  
+  };  
 }
 
 
 
 Pebble.addEventListener("ready",
-                         function(e)){                           console.log("connect" + e.ready());
+                         function(e){                           
+                           console.log("connect" + e.ready());
                             console.log("connect" + e.ready);
                          });
